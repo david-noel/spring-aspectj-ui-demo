c9afecfe4a49a47c6c84826edc6d38d39f99316a
dave <dave@devbury.com>
2013-10-31 22:13:58 -0400

    remove unused panel

--- src/main/java/ui/ManagedBeanDialog.java
+++ src/main/java/ui/ManagedBeanDialog.java
@@ -16,7 +16,7 @@ public class ManagedBeanDialog extends JDialog {
     @Inject
     private Message message;
 
-    @Value("${dialogTitle:defaultTitle}")
+    @Value("${dialogTitle:dialogTitle}")
     private String dialogTitle;
 
     @PostConstruct
@@ -27,8 +27,6 @@ public class ManagedBeanDialog extends JDialog {
         ManualButton mButton = Application.wire(new ManualButton());
         FancyButton fButton = new FancyButton();
         Application.wire(fButton);
-        JPanel p = new JPanel();
-        p.setLayout(new BorderLayout());
         add(mButton, BorderLayout.NORTH);
         add(fButton, BorderLayout.SOUTH);
         pack();
